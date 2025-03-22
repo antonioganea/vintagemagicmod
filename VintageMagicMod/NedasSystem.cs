6cdabc05106ec190fdb8490b845c175072aec8ed
Antonio Ganea <antonio.ganea@yahoo.com>
2025-03-22 18:50:24 +0200

    Distributed files for everyone in the project -- temporary solution

---
+++ VintageMagicMod/NedasSystem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+
+namespace VintageMagicMod
+{
+    internal class NedasSystem : TemporarySystem
+    {
+        public void Start(ICoreAPI api)
+        {
+
+        }
+        public void StartServerSide(ICoreServerAPI api)
+        {
+
+        }
+        public void StartClientSide(ICoreClientAPI api)
+        {
+
+        }
+    }
+}
